7d8f3a38b802044c265e4aa0f71b0879caef6693
luanlrf <luan.r.ferraz@gmail.com>
2018-10-12 20:33:39 -0300

    Adicionar arquivos de projeto.

---
+++ EconomiDay/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace EconomiDay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tsalario = new System.Windows.Forms.TextBox();
+            this.tdicidio = new System.Windows.Forms.TextBox();
+            this.tcalculo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Salario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NovoSalario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dicidio em %";
+            // 
+            // tsalario
+            // 
+            this.tsalario.Location = new System.Drawing.Point(186, 36);
+            this.tsalario.Name = "tsalario";
+            this.tsalario.Size = new System.Drawing.Size(100, 20);
+            this.tsalario.TabIndex = 3;
+            // 
+            // tdicidio
+            // 
+            this.tdicidio.Location = new System.Drawing.Point(186, 67);
+            this.tdicidio.Name = "tdicidio";
+            this.tdicidio.Size = new System.Drawing.Size(100, 20);
+            this.tdicidio.TabIndex = 4;
+            // 
+            // tcalculo
+            // 
+            this.tcalculo.Location = new System.Drawing.Point(186, 100);
+            this.tcalculo.Name = "tcalculo";
+            this.tcalculo.Size = new System.Drawing.Size(100, 20);
+            this.tcalculo.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tcalculo);
+            this.Controls.Add(this.tdicidio);
+            this.Controls.Add(this.tsalario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Economi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tsalario;
+        private System.Windows.Forms.TextBox tdicidio;
+        private System.Windows.Forms.TextBox tcalculo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EconomiDay/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EconomiDay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double salario, dicidio, calculo, valordicidio,valorpercent;
+            salario = double.Parse(tsalario.Text);
+            dicidio = double.Parse(tdicidio.Text);
+            valordicidio = dicidio*salario;
+            calculo = valordicidio + salario;
+            tcalculo.Text = calculo.ToString();
+        }
+    }
+}
